aea10cff58df81565d42cefe16b5405ac4e633b7
Max0490 <seagate90@mail.ru>
2022-09-14 17:58:19 +0300

    added second test (remane)

--- src/test/java/seagate/ru/ExampleTest.java
+++ src/test/java/seagate/ru/ExampleTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
 
     @Test
-    void exampleTest0() {
+    void exampleTest1() {
         Assertions.assertTrue(2 > 1);
     }
 
     @Test
-    void exampleTest1() {
+    void exampleTest2() {
         Assertions.assertTrue(3 > 2);
     }
 }
